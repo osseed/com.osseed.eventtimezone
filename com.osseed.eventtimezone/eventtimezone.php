3cdaac1baafcb903108db878719c1facb206c36e
madhavimalgaonkar <madhavim2694@gmail.com>
2018-02-27 13:49:41 +0530

    Changed the timezone listing [#155352407]

--- com.osseed.eventtimezone/eventtimezone.php
+++ com.osseed.eventtimezone/eventtimezone.php
@@ -128,8 +128,7 @@ function eventtimezone_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  */
 function eventtimezone_civicrm_buildForm($formName, &$form) {
   if($formName == 'CRM_Event_Form_ManageEvent_EventInfo') {
-    $timezone_identifiers = DateTimeZone::listIdentifiers();
-    $options['_none'] = 'Select Timezone';
+    $timezone_identifiers = DateTimeZone::listIdentifiers(DateTimeZone::AMERICA);
     foreach ($timezone_identifiers as $key => $value) {
       $options[$value] = $value;
     }
@@ -198,22 +197,14 @@ function eventtimezone_civicrm_alterContent( &$content, $context, $tplName, &$ob
     SELECT timezone FROM civicrm_event WHERE id = $object->_id";
     $timezone = CRM_Core_DAO::singleValueQuery($query);
     // Convert date
-    if ($timezone != '_none') {
-      $start_date_timestamp = new DateTime($start_date, new DateTimeZone($timezone));
-      $start_date_st = date_format($start_date_timestamp, 'M jS Y g:iA T');
-      $content = str_replace("event_start_date", $start_date_st, $content);
+    $start_date_timestamp = new DateTime($start_date, new DateTimeZone($timezone));
+    $start_date_st = date_format($start_date_timestamp, 'M jS Y g:iA T');
+    $content = str_replace("event_start_date", $start_date_st, $content);
 
-      if ($end_date) {
-        $end_date_timestamp = new DateTime($end_date, new DateTimeZone($timezone));
-        $end_date_st = date_format($end_date_timestamp, 'M jS Y g:iA T');
-        $content = str_replace("event_end_date", $end_date_st, $content);
-      }
-    }
-    else {
-      $content = str_replace("event_start_date", $start_date, $content);
-      if ($end_date) {
-        $content = str_replace("event_end_date", $end_date, $content);
-      }
+    if ($end_date) {
+      $end_date_timestamp = new DateTime($end_date, new DateTimeZone($timezone));
+      $end_date_st = date_format($end_date_timestamp, 'M jS Y g:iA T');
+      $content = str_replace("event_end_date", $end_date_st, $content);
     }
   }
 }
